f4318a7f6aa0558b3e2da8b9c50ef1ce9f98ef1c
dimitheodoro <42531867+dimitheodoro@users.noreply.github.com>
2022-07-07 06:32:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ if st.button("Press for Detection of Lung Nodules"):
     keep = nms (boxes=boxes,scores=scores,iou_threshold=0.3)
     scores =scores.detach().cpu().numpy()
     
-    print( "len :",len(boxes.detach().cpu().numpy())!)
+    print( "len :",len(boxes.detach().cpu().numpy()))
     
     if len(boxes.detach().cpu().numpy())!=0:
         image_arr =np.stack((image_arr,image_arr,image_arr),axis=2)
