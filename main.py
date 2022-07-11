aa38ed43c1c17a0cd6c90ed2d1a66764db5078a9
dimitheodoro <42531867+dimitheodoro@users.noreply.github.com>
2022-07-11 12:33:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ import gdown
 
 
 
-@st.experimental_singleton
+@st.experimental_memo
 def download_weights(url):
     gdown.download(url, "weight_path", quiet=False)
 
 
 
-@st.experimental_singleton
+@st.experimental_memo
 def load_model():
     print(" MODEL LOADED !!!")
     return torch.load("weight_path", map_location=device)
